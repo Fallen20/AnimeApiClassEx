743ea1cb83f28a6ed21c29482e9a23187d9c0de0
Fallen20 <montsegordillo20@hotmail.com>
2021-11-22 16:24:16 +0100

    cambio Lunes tutoria

--- src/main/java/repository/FileRepository.java
+++ src/main/java/repository/FileRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.UUID;
 //consulta=select a from
 public interface FileRepository extends JpaRepository<FileTable, UUID> {//quien le da la informacion de la tabla. Es quien hace las consultas
-    //la tabla y el tipo de dato de la clave primaria
+    // la tabla y el tipo de dato de la clave primaria
     //con interface genera la clase por cada consulta
 
     @Query("select fileid from FileTable")
