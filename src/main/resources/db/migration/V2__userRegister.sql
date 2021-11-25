a64e7e56774cb26b10bbf3f3529ec51870d7623e
Fallen20 <montsegordillo20@hotmail.com>
2021-11-25 21:01:47 +0100

    arreglado todo en teoria

--- src/main/resources/db/migration/V2__userRegister.sql
+++ src/main/resources/db/migration/V2__userRegister.sql
@@ -1,2 +1,2 @@
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
-  INSERT INTO usser (username, password) VALUES ('user', crypt('pass', gen_salt('bf')));
+  INSERT INTO users (username, password) VALUES ('user', crypt('pass', gen_salt('bf')));
